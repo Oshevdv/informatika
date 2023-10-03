0a5c528de9ce9c70430f2ae2d7ae542f5a9d8174
Oshevdv <144201796+Oshevdv@users.noreply.github.com>
2023-10-03 20:06:47 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,11 +1,1 @@
-#import math as m
-from math import *
 
-x = float(input())
-y = float(input())
-z = float(input())
-
-a = ((x * y)**2 + ((x+y)/2)) / ((x**0.5) - (y**0.5))
-b = 2 * sin(x) + ((2*y + log1p(z))**0.5)
-
-print(a,b)
